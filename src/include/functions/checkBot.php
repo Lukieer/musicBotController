c92265b551c3fd014ac3ec9807aca254ea5a8c7f
mskarbek <66939603+Lukieer@users.noreply.github.com>
2021-11-28 22:15:49 +0100

    Update checkBot.php

--- src/include/functions/checkBot.php
+++ src/include/functions/checkBot.php
@@ -22,148 +22,54 @@
  * 
  * 
  */ 
-    class guildsController
+    class checkBot
     {        
         public function init($ts, $cfg, $clients, $Main, $TeamSpeak)
         {
-            $channelList = $ts->channelList()['data'];
-            foreach($channelList as $channel)
+            $count = 0;
+            $bots = array();
+            foreach($clients as $client)
             {
-                $channels[$channel['cid']] = $channel;
+                $clGroups = explode(",",$client['client_servergroups']);
+                if($client['client_type'] == 0 && $Main->inGroup($cfg['musicbot_groups'], $clGroups) && $client['cid'] == $cfg['musicbot_cid'])
+                {
+                    $bots[] = $client;
+                    $count++;
+                    if($count > $cfg['max_bots'])
+                    {
+                        $ts->sendMessage(1, $client['clid'], $cfg['disconnect_command']);
+                        $count--;
+                    }
+                }
             }
-            $channelList = $channels;
-            unset($channels);
-
-            switch($cfg['mode'])
+            if(count($bots) < $cfg['min_bots'])
             {
-                case "openapps":
-                    try {
-                        $pdo = new PDO("mysql:host=" . $cfg['database']['address'] . ";dbname=" . $cfg['database']['database'] . "", $cfg['database']['login'], $cfg['database']['password']);
-                        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                    } catch(PDOException $e) {
-                        throw new Exception("Nie udało się połączyć z bazą danych!");
-                        return false;
-                    }
-
-                    $request = $pdo->query("SELECT `cids`, `name`,`type`, `teleport` FROM ".$cfg['database']['table']);
-                    $guilds = $request->fetchAll(PDO::FETCH_ASSOC);
-
-                    foreach($guilds as $guild)
+                $ts3ab = new Lukieer\TS3AudioBot\Api\Instance($cfg['ts3audiobot']['url'], $cfg['ts3audiobot']['login'], $cfg['ts3audiobot']['password']);
+                $botList = $ts3ab->botList();
+                $ok = false;
+                foreach($botList as $bot)
+                {
+                    if($bot['Status'] == 0)
                     {
-                        $clans[$guild['name']] = ['cids' => explode(", ",$guild['cids']), 'type' => $guild['type'], 'channel' => $guild['teleport']];
+                        $ts3ab->connect($bot['Name']);
+                        $ok = true;
+                        break;
                     }
+                }
+                if(!$ok)
+                {
 
-            }
-            if(!empty($clans))
-            {
-                foreach($clans as $name => $clan)
-                 {
-                    $channels = array();
-                    $first_channel = $clan['cids'][0];
-                    $last_channel = $clan['cids'][array_key_last($clan['cids'])];
-                    foreach($clan['cids'] as $cid)
+                    foreach($clients as $bot)
                     {
-
-                        foreach($channelList as $channel)
+                        if($Main->inGroup($cfg['musicbot_groups'], explode(",",$bot['client_servergroups'])))
                         {
-                            if($channel['cid'] == $cid)
-                            {
-                                $channels[$channel['cid']] = $channel;
-                            }
-                        }
-
-                        foreach($Main->getSubChannels($channelList, $cid) as $channel)
-                        {
-                            $channels[$channel['cid']] = $channel;
-                        }
-
-                    }
-
-                    $bots = [];
-                    foreach($channels as $channel)
-                    {
-                        foreach($Main->channelClientList($clients, $channel['cid']) as $client)
-                        {
-                            if($Main->inGroup($cfg['musicbot_groups'], explode(",",$client['client_servergroups'])))
-                            {
-                                $bots[] = $client;
-                            }
-                        }
-                    }
-
-
-                    $i = 0;
-                    foreach($bots as $bot)
-                    {
-                        $i++;
-                        $correctName = str_replace(["[NAME]", "[num]"], [$name, $i], $cfg['botName']);
-                        if($correctName != $bot['client_nickname'])
-                        {
-                            echo 1;
-                            $ts->sendMessage(1, $bot['clid'], str_replace(["[NAME]", "[num]"], [$name, $i], $cfg['name_command'] . " " . $cfg["botName"]));
-                        }
-                    }
-
-                    if(isset($cfg['sectors'][$clan['type']]))
-                    {
-                        if($cfg['sectors'][$clan['type']] != count($bots))
-                        {
-
-                            if(count($bots) > $cfg['sectors'][$clan['type']])
-                            {
-                                $over = count($bots) - $cfg['sectors'][$clan['type']];
-                            } else {
-                                $needed = $cfg['sectors'][$clan['type']] - count($bots);
-                            }
-
-
-                            if(isset($over))
-                            {
-                                foreach($bots as $bot)
-                                {
-                                    if($over != 0)
-                                    {
-                                        if($ts->clientMove($bot['clid'], $cfg['musicbot_channel'])['success'])
-                                        {
-                                            foreach($clients as $key => $client)
-                                            {
-                                                if($client['clid'] == $bot['clid'])
-                                                {
-                                                    $clients[$key]['cid'] = $cfg['musicbot_channel'];
-                                                }
-                                            }
-                                            $ts->sendMessage(1, $bot['clid'], str_replace("[db]", $client['client_database_id'], $cfg['backName']));
-                                            $over--;
-                                        }
-                                    } else {
-                                        break;
-                                    }
-                                }
-                            }
-                            if(isset($needed))
-                            {
-                                $i = 0;
-                                foreach($Main->channelClientList($clients, $cfg['musicbot_channel']) as $client)
-                                {
-                                    if($needed != 0)
-                                    {
-                                        echo 4;
-                                        $i++;
-                                        $ts->sendMessage(1, $client['clid'], str_replace(["[NAME]", "[num]"], [$name, count($bots)+$i], $cfg['setName']));
-                                        $ts->clientMove($client['clid'], $clan['channel']);
-                                        foreach($clients as $key => $bot)
-                                        {
-                                            if($client['clid'] == $bot['clid'])
-                                            {
-                                                $clients[$key]['cid'] = $clan['channel'];
-                                            }
-                                        }
-                                        $needed--;
-                                    } else {
-                                        break;
-                                    }
-                                }
-                            }
+                            $ts->sendMessage(1, $bot['clid'], "!bot connect to ".$cfg['teamspeak']);
+                            $chat = $ts->readChatMessage('textprivate')['data'];
+                            var_dump($chat);
+                            $matches = explode("Id: ", $chat['msg'])[1];
+                            $matches = explode(" Name:",$matches)[0];
+                            $ts3ab->bot($matches)->save('AutoSave-'.$bot['client_database_id'].substr(md5(microtime()),rand(0,26),3));
+                            break;
                         }
                     }
                 }
